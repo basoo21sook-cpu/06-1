d55fe8d30822c18db4368e441955496c578bf9e9
basoo21sook-cpu <basoo21sook@gmail.com>
2025-10-08 14:59:58 +0900

    06-4

--- main.c
+++ main.c
@@ -3,34 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-// 두 개의 정수를 더하는 함수
-int sumTwo(int a, int b)
+int square(int a)
 {
-    return (a + b);
+	return (a*a);
 }
-
-// 정수의 제곱을 계산하는 함수
-int square(int n)
-{
-    return (n * n);
-}
-
-// 두 개의 정수 중 큰 수를 반환하는 함수
-int get_max(int a, int b)
-{
-    if (a > b)
-        return a;
-    else
-        return b;
-}
-
 int main(int argc, char *argv[])
 {
-    printf("sum two result is %i\n", sumTwo(2, 3));   
-    printf("square result is %i\n", square(5));      
-    printf("getmax result is %i\n", get_max(10, 13));
+    int a = 2;
+    a= square(a);    
+    printf("a = %i\n", a); 
 
     return 0;
 }
+
 
