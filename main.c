285b887de9586e9a866762a3e10afac491db249f
basoo21sook-cpu <basoo21sook@gmail.com>
2025-10-08 14:38:12 +0900

    06-2

--- main.c
+++ main.c
@@ -4,21 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-void printstar(int n)    // 매개변수 n에 출력할 개수 전달
+void func(void)
 {
-    int i;
-    for (i = 0; i < n; i++)
-        printf("*");
-    printf("\n");
+    int x;
+    printf("func x is at %p\n", &x);   // func 안의 x 주소 출력
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    int i;
-
-    printstar(10);
-    printstar(10);
-    printstar(10);
-
+    int x;
+    printf("main x is at %p\n", &x);   // main 안의 x 주소 출력
+    func();                            // func() 호출
     return 0;
 }
+
